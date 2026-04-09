dca35116b48015721aa952ac9b94b2ab3d9defcc
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-10 00:24:25 +0530

    basics done

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const scene = new THREE.Scene()
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+const camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000)
+camera.position.z = 3
+scene.add(camera)
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
 
-camera.position.z = 5;
+const cubeMaterial = new THREE.MeshStandardMaterial({
+  color: 0x00ff00
+})
 
-function animate( time ) {
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cube)
 
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 1000;
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(2, 2, 2)
+scene.add(light)
 
-  renderer.render( scene, camera );
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(innerWidth, innerHeight)
+document.body.appendChild(renderer.domElement)
 
-  requestAnimationFrame(animate)
-
+function animate(){
+  cube.rotation.x += 0.01
+  cube.rotation.y += 0.01
+  cube.rotation.z += 0.01
+  
+  renderer.render(scene, camera)
 }
 
-animate()
+renderer.setAnimationLoop(animate)
