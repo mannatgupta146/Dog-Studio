081fc72272e8d7f56251add86886b51f6f1565a0
mannatgupta146 <mannatgupta146@gmail.com>
2026-04-12 00:17:28 +0530

    normals texture added

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,18 +1,34 @@
 import React from 'react'
+import * as THREE from 'three'
 import { Canvas, useThree } from '@react-three/fiber'
-import { OrbitControls, useGLTF } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 import { DirectionalLight } from 'three'
 
 const Dog = () => {
 
   const model = useGLTF('/model/dog.drc.glb')
   useThree(({camera, scene, gl}) => {
-    camera.position.z = 0.9
+    camera.position.z = 0.75
   })
+
+  const textures = useTexture({
+    normalMap: "/dog_normals.jpg"
+  })
+
+  textures.normalMap.flipY = false
+
+  model.scene.traverse((child) => {
+    if(child.name.includes('DOG')){
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap: textures.normalMap,
+      })
+    }
+  })
+
 
   return (
       <>
-      <primitive object={model.scene} position={[0.2, -0.5, 0]} rotation={[0, Math.PI/4, 0]}/>
+      <primitive object={model.scene} position={[0.2, -0.5, 0]} rotation={[0, Math.PI/3.8, 0]}/>
       <directionalLight position={[0, 5, 5]} intensity={10} color="#ffffff" />
       <OrbitControls/>
       </>
